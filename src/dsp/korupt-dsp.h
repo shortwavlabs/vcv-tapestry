b8ee095ac9c687f9a8d04f5243ffb60a4aaeef68
Stephane P. Pericat <contact@pericat.net>
2026-07-16 15:49:48 -0500

    fix GHA windows

--- src/dsp/korupt-dsp.h
+++ src/dsp/korupt-dsp.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#ifndef M_SQRT2
+#define M_SQRT2 1.41421356237309504880
+#endif
+
 #include <dsp/filter.hpp>
 #include <math.hpp>
 
